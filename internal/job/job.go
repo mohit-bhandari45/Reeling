90611ff50187a0fb1543305e29d43fee14ff6d9e
mohit-bhandari45 <mohitbhandari852@gmail.com>
2026-08-30 20:05:03 +0530

    defined job struct

--- internal/job/job.go
+++ internal/job/job.go
@@ -11,3 +11,12 @@ const (
 	StatusFailed Status = "failed"
 )
 
+type Job struct {
+	ID string `json:"id`
+	InputKey string `json:"input_key"`
+	OutputKeys []string `json:"output_keys,omitempty"`
+	status Status `json:"status"`
+	Error string `json:"error,omitempty"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
