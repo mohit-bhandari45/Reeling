095e6cfd3ae1c8df7521ee6ba710304af1849834
mohit-bhandari45 <mohitbhandari852@gmail.com>
2026-08-30 21:49:45 +0530

    local.go file added

---
+++ internal/storage/local.go
@@ -0,0 +1,44 @@
+package storage
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"uuid"
+
+	"github.com/google/uuid"
+)
+
+type LocalDisk struct {
+	BaseDir string
+}
+
+func NewLocalDisk(baseDir string) (*LocalDisk, error) {
+	if err := os.MkdirAll(baseDir, 0o755); err != nil {
+		return nil, err
+	}
+
+	return &LocalDisk{BaseDir: baseDir}, nil
+}
+
+func (d *LocalDisk) Save(name string, r io.Reader) (string, error) {
+	key := fmt.Sprintf("%s-%s", uuid.NewString(), filepath.Base(name));
+	path := filepath.Join(d.BaseDir, key);
+
+	f, err := os.Create(path);
+	if err != nil {
+		return "", err;
+	}
+	defer f.Close();
+
+	if _, err := io.Copy(f, r); err != nil {
+		return "", err;
+	}
+
+	return key, nil;
+}
+
+func (d *LocalDisk) Open(key string) (io.ReadCloser, error) {
+	return os.Open(filepath.Join(d.BaseDir, key));
+}
