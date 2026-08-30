2a184c4fc8c6f1c5e1745b8cd1d6c606fbbf8f9c
mohit-bhandari45 <mohitbhandari852@gmail.com>
2026-08-30 23:12:48 +0530

    ffmpeg setup

---
+++ internal/ffmpeg/ffmpeg.go
@@ -0,0 +1,45 @@
+package ffmpeg
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+)
+
+type Runner struct {
+	WorkDir string
+}
+
+func NewRunner(workDir string) *Runner {
+	return &Runner{WorkDir: workDir}
+}
+
+func (r *Runner) Transcode(ctx context.Context, input, output, preset string) error {
+	cmd := exec.CommandContext(ctx, "ffmpeg",
+		"-i", input,
+		"-c:v", "libx264",
+		"-preset", preset,
+		"-c:a", "aac",
+		"-y", output,
+	)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("ffmpeg transcode failed: %w\n%s", err, out)
+	}
+	return nil
+}
+
+func (r *Runner) Thumbnail(ctx context.Context, input, output string, atSeconds int) error {
+	cmd := exec.CommandContext(ctx, "ffmpeg",
+		"-ss", fmt.Sprintf("%d", atSeconds),
+		"-i", input,
+		"-frames:v", "1",
+		"-y", output,
+	)
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("ffmpeg thumbnail failed: %w\n%s", err, out)
+	}
+	return nil
+}
