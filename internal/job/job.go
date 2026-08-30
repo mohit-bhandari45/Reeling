2420dd227db4a22dcfeb51da60d6d41fe465dd40
mohit-bhandari45 <mohitbhandari852@gmail.com>
2026-08-30 20:05:53 +0530

    small fix

--- internal/job/job.go
+++ internal/job/job.go
@@ -12,10 +12,10 @@ const (
 )
 
 type Job struct {
-	ID string `json:"id`
+	ID string `json:"id"`
 	InputKey string `json:"input_key"`
 	OutputKeys []string `json:"output_keys,omitempty"`
-	status Status `json:"status"`
+	Status Status `json:"status"`
 	Error string `json:"error,omitempty"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
