fc96a884dc856c0f57e7c96bf77e56e5c458f929
mohit-bhandari45 <mohitbhandari852@gmail.com>
2026-08-31 15:31:33 +0530

    pool struct

--- internal/worker/pool.go
+++ internal/worker/pool.go
@@ -1,2 +1,15 @@
 package worker
 
+import (
+	"github.com/mohit-bhandari45/Reeling/internal/ffmpeg"
+	"github.com/mohit-bhandari45/Reeling/internal/job"
+	"github.com/mohit-bhandari45/Reeling/internal/storage"
+)
+
+type Pool struct {
+	jobs chan *job.Job
+	store job.Store
+	storage storage.Storage
+	ffmpeg *ffmpeg.Runner
+}
+
