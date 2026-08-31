6bec77aa8907d6f51b51e60260e7b70a2fdd59e5
mohit-bhandari45 <mohitbhandari852@gmail.com>
2026-08-31 15:45:55 +0530

    Newpool added

--- internal/worker/pool.go
+++ internal/worker/pool.go
@@ -9,7 +9,27 @@ import (
 type Pool struct {
 	jobs chan *job.Job
 	store job.Store
-	storage storage.Storage
+	files storage.Storage
 	ffmpeg *ffmpeg.Runner
 }
 
+func NewPool(size int, store job.Store, files storage.Storage, runner *ffmpeg.Runner) *Pool {
+	p := &Pool{
+		jobs: make(chan *job.Job, 100),
+		store: store,
+		files: files,
+		ffmpeg: runner,
+	}
+
+	for i:=0;i<size;i++ {
+		go p.startWorker(i);
+	}
+
+	return p;
+}
+
+func (p *Pool) startWorker(id int) {
+	for j := range p.jobs {
+		p.process(id, j);
+	}
+}
