0ab08997e3bd3e301b08abe16993151f26a6e7fe
mohit-bhandari45 <mohitbhandari852@gmail.com>
2026-08-31 10:49:49 +0530

    creating a localdisk funciton

--- internal/storage/local.go
+++ internal/storage/local.go
@@ -1,3 +1,19 @@
 package storage
 
-type LocalStorage
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+type LocalDisk struct {
+	BaseDir string
+}
+
+func NewLocalDisk(baseDir string) (*LocalDisk, error) {
+	if err := os.MkdirAll(baseDir); err!=nil {
+		return nil, err;
+	}
+	return &LocalDisk{BaseDir: baseDir}
+}
+
