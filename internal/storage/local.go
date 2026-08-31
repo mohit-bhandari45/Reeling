47b4d73d7930fd161ce2d74197265b68c0ef2fde
mohit-bhandari45 <mohitbhandari852@gmail.com>
2026-08-31 10:54:24 +0530

    save function

--- internal/storage/local.go
+++ internal/storage/local.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"uuid"
+
+	"github.com/google/uuid"
 )
 
 type LocalDisk struct {
@@ -11,9 +15,24 @@ type LocalDisk struct {
 }
 
 func NewLocalDisk(baseDir string) (*LocalDisk, error) {
-	if err := os.MkdirAll(baseDir); err!=nil {
+	if err := os.MkdirAll(baseDir, 0o755); err!=nil {
 		return nil, err;
 	}
-	return &LocalDisk{BaseDir: baseDir}
+	return &LocalDisk{BaseDir: baseDir}, nil
 }
 
+func (d *LocalDisk) Save(name string, r io.Reader) (string, error) {
+	key := fmt.Sprintf("%s-%s", uuid.NewString(), filepath.Base(name));
+	path := filepath.Join(d.BaseDir, key);
+
+	f, err := os.Create(path);
+	if err != nil {
+		return "", err
+	}
+	defer f.Close();
+
+	if _, err := io.Copy(f, r); err != nil {
+		return "", nil
+	}
+	return key, nil;
+}
