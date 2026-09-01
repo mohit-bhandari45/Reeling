f35a410c30007cda9785b54acd6171bb9c6061da
mohit-bhandari45 <mohitbhandari852@gmail.com>
2026-09-01 20:47:34 +0530

    test to test the bucket creation and putting the file

--- internal/storage/s3_manual_test.go
+++ internal/storage/s3_manual_test.go
@@ -2,6 +2,7 @@ package storage
 
 import (
 	"context"
+	"os"
 	"testing"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -17,4 +18,30 @@ func TestS3ConnManual(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestS3SaveManual(t *testing.T) {
+	client, err := NewS3Client("http://localhost:9000", "reeling", "reelingreeling")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx := context.Background()
+	if err := EnsureBucket(ctx, client, "reeling-videos"); err != nil {
+		t.Fatal(err)
+	}
+
+	s3Storage := NewS3Storage(client, "reeling-videos")
+
+	file, err := os.Open("sample.mp4")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer file.Close()
+
+	key, err := s3Storage.Save("sample.mp4", file)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Logf("uploaded with key: %s", key)
 }
