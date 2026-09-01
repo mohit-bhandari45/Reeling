91a8f4c44c21e27e89b737025647313d583c156f
mohit-bhandari45 <mohitbhandari852@gmail.com>
2026-09-01 20:45:47 +0530

    added save method ob s3storage

--- internal/storage/s3.go
+++ internal/storage/s3.go
@@ -3,11 +3,14 @@ package storage
 import (
 	"context"
 	"fmt"
+	"io"
+	"path/filepath"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/google/uuid"
 )
 
 // creating a new client
@@ -48,7 +51,7 @@ func EnsureBucket(ctx context.Context, client *s3.Client, bucket string) error {
 	if err != nil {
 		return fmt.Errorf("failed to create bucket: %w", err)
 	}
-	
+
 	return nil;
 }
 
@@ -62,4 +65,21 @@ func NewS3Storage(client *s3.Client, bucket string) *S3Storage {
 		client: client,
 		bucket: bucket,
 	}
+}
+
+// save function to match Storage interface
+func (s *S3Storage) Save(name string, r io.Reader) (string, error) {
+	key := fmt.Sprintf("%s-%s", uuid.NewString(), filepath.Base(name));
+
+	_, err := s.client.PutObject(context.Background(), &s3.PutObjectInput{
+		Bucket: aws.String(s.bucket),
+		Key: aws.String(key),
+		Body: r,
+	});
+
+	if err != nil {
+		return "", fmt.Errorf("failed to upload to s3: %w", err)
+	}
+
+	return key, nil
 }
