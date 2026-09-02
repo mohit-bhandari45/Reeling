f0e21f53c67c27ae9a91c73054d8248db6384846
mohit-bhandari45 <mohitbhandari852@gmail.com>
2026-09-02 09:30:19 +0530

    test for queue

--- internal/queue/queue_manual_test.go
+++ internal/queue/queue_manual_test.go
@@ -39,3 +39,36 @@ func TestListStreamsManual(t *testing.T) {
 		t.Logf("found stream: %s", stream.Config.Name)
 	}
 }
+
+func TestPublishConsumeManual(t *testing.T) {
+	nc, js, err := NewConn("nats://localhost:4222")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer nc.Close()
+
+	ctx := context.Background()
+
+	if err := EnsureStream(ctx, js); err != nil {
+		t.Fatal(err)
+	}
+
+	if err := Publish(ctx, js, []byte(`{"id":"test-msg-1"}`)); err != nil {
+		t.Fatal(err)
+	}
+
+	cons, err := CreateConsumer(ctx, js)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	msgs, err := cons.Fetch(1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	for msg := range msgs.Messages() {
+		t.Logf("received: %s", string(msg.Data()))
+		msg.Ack()
+	}
+}
